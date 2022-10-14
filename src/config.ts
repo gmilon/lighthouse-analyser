6d8dd47429def2f230d8f20d40b608ce4b9a60d7
Palani Kamaraj <92712865+Palani-K@users.noreply.github.com>
2022-10-14 17:02:24 +0400

    getPages function is updated

--- src/config.ts
+++ src/config.ts
@@ -3,12 +3,14 @@ import { siteUrls } from "./urls";
 config();
 
 /**
-* *Get the pages based on start and end values from script `yarn start START_INDEX:END_INDEX` 
-* * process.argv - return use the array of script arguments
-*/
+ * *Get the pages based on start and end values from script `yarn start START_INDEX:END_INDEX`
+ * * process.argv - return use the array of script arguments
+ */
 const getPages = () => {
-  const pages = process.argv.at(-1).split(":"); 
-  return siteUrls.paths.slice(pages[String(0)], pages[String(1)]);
+  const pages = process.argv.at(-1).split(":");
+  const start = parseInt(pages[0]);
+  const end = parseInt(pages[1]);
+  return siteUrls.paths.slice(start, end);
 };
 
 export default {
@@ -22,8 +24,8 @@ export default {
   google: {
     sheetId: process.env.GOOGLE_SHEET_ID, // ex: aef213fsdfg
     token: {
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // ex: -----BEGIN PRIVATE KEY-----
-      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL // ex: sheet....@..serviceaccount.com
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"), // ex: -----BEGIN PRIVATE KEY-----
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL, // ex: sheet....@..serviceaccount.com
     },
   },
 };
